f2c55515842c889aad6652ec352357e240c1a416
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-12 17:37:12 +0530

    100 hours Calculation

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,21 +6,21 @@ class Program
     public const int IS_FULL_TIME = 2;
     public const int EMP_RATE_PER_HOUR = 20;
     public const int NUM_OF_WORKING_DAYS = 20;
+    public const int MAX_HRS_IN_MONTH = 100;
     public static void Main(string[] args)
     {
 
 
         int empHrs = 0;
-        int empWage = 0;
-        int totalEmpWage = 0;
-
-        
-
-        for(int day = 0; day < NUM_OF_WORKING_DAYS; day++)
+        int empWage = 0; 
+        int totalWorkingDays = 0;
+        int totalEmphrs = 0;
+        while(totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
+            totalWorkingDays++;
             Random random = new Random();
             int empCheck = random.Next(0, 3);
-            switch(empCheck)
+            switch (empCheck)
             {
                 case IS_PART_TIME:
                     empHrs = 4;
@@ -32,25 +32,14 @@ class Program
                     empHrs = 0;
                     break;
             }
-            empWage = empHrs * EMP_RATE_PER_HOUR;
-            totalEmpWage += empWage;
-            Console.WriteLine("Emp Wage : "+empWage);
+            totalEmphrs += empHrs;
+            Console.WriteLine("Days: " +totalWorkingDays + "Emp Hrs "+ empHrs); ;
+
 
         }
+        int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Emp Wage : " +totalEmpWage);
 
  
-        //if(empCheck == IS_PART_TIME)
-        //{
-        //    empHrs = 4;
-        //}
-        //else if(empCheck == IS_FULL_TIME)
-        //{
-        //    empHrs = 8;
-        //}
-        //else
-        //{
-        //    empHrs=0;
-        //}
     }
 }
