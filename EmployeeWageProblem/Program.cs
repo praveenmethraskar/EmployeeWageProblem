207e33cec28e5d01ebb6be53b07da4651a80be06
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-12 17:40:40 +0530

    UC7 using method

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -7,15 +7,14 @@ class Program
     public const int EMP_RATE_PER_HOUR = 20;
     public const int NUM_OF_WORKING_DAYS = 20;
     public const int MAX_HRS_IN_MONTH = 100;
-    public static void Main(string[] args)
+    public static int computeEmpWage()
     {
 
-
         int empHrs = 0;
-        int empWage = 0; 
+        int empWage = 0;
         int totalWorkingDays = 0;
         int totalEmphrs = 0;
-        while(totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
         {
             totalWorkingDays++;
             Random random = new Random();
@@ -39,7 +38,10 @@ class Program
         }
         int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOUR;
         Console.WriteLine("Total Emp Wage : " +totalEmpWage);
-
- 
+        return totalEmpWage;
+    }
+    public static void Main(string[] args)
+    {
+        computeEmpWage();
     }
 }
