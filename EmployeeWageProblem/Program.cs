c7ee8fc93936af9bd2ea39def1b4c8af20b6e8d6
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-12 17:49:51 +0530

    Multiple companies

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -4,17 +4,14 @@ class Program
 {
     public const int IS_PART_TIME = 1;
     public const int IS_FULL_TIME = 2;
-    public const int EMP_RATE_PER_HOUR = 20;
-    public const int NUM_OF_WORKING_DAYS = 20;
-    public const int MAX_HRS_IN_MONTH = 100;
-    public static int computeEmpWage()
+    
+    public static int computeEmpWage(string company, int empRatePerHour, int numOfWorkingDays, int maxHourSPerMonth)
     {
 
-        int empHrs = 0;
-        int empWage = 0;
+        int empHrs = 0; 
         int totalWorkingDays = 0;
         int totalEmphrs = 0;
-        while (totalEmphrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS)
+        while (totalEmphrs <= maxHourSPerMonth && totalWorkingDays < numOfWorkingDays)
         {
             totalWorkingDays++;
             Random random = new Random();
@@ -33,15 +30,14 @@ class Program
             }
             totalEmphrs += empHrs;
             Console.WriteLine("Days: " +totalWorkingDays + "Emp Hrs "+ empHrs); ;
-
-
         }
-        int totalEmpWage = totalEmphrs * EMP_RATE_PER_HOUR;
+        int totalEmpWage = totalEmphrs * empRatePerHour;
         Console.WriteLine("Total Emp Wage : " +totalEmpWage);
         return totalEmpWage;
     }
     public static void Main(string[] args)
     {
-        computeEmpWage();
+        computeEmpWage("Dmart",20,2,10);
+        computeEmpWage("Jio", 10, 5, 30);
     }
 }
