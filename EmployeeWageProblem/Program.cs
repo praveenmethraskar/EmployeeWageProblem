f0ef9b970cddeaae0e770b5b5b46bd44b973be85
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-12 17:15:14 +0530

    Emp wage calculation

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        int IS_FULL_TIME = 1;
+        int EMP_RATE_PER_HOUR = 20;
+
+        int empHrs = 0;
+        int empWage = 0;
+
+        Random random = new Random();
+
+        int empCheck = random.Next(0,2);
+        if(empCheck == IS_FULL_TIME)
+        {
+            empHrs = 8;
+        }
+        else
+        {
+            empHrs=0;
+        }
+        empWage = empHrs * EMP_RATE_PER_HOUR;
+        Console.WriteLine("Emp Wage : "+empWage);
+    }
+}
