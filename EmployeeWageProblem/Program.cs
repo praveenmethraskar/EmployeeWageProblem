9f6f7abca0fbbaa4fd95b7cb9c21c3cc92c53a78
praveenmethraskar <praveenmethraskar@gmail.com>
2022-08-13 18:04:37 +0530

    Ability to manage employee wage of multiple companies

--- EmployeeWageProblem/Program.cs
+++ EmployeeWageProblem/Program.cs
@@ -6,11 +6,16 @@ class Program
     
     public static void Main(string[] args)
     {
-        EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart",20,2,10);
-        EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance",10,4,20);
-        dMart.computeEmpWage();
-        Console.WriteLine(dMart.toString());
-        reliance.computeEmpWage();
-        Console.WriteLine(reliance.toString());
+        //EmpWageBuilderObject dMart = new EmpWageBuilderObject("Dmart",20,2,10);
+        //EmpWageBuilderObject reliance = new EmpWageBuilderObject("Reliance",10,4,20);
+        //dMart.computeEmpWage();
+        //Console.WriteLine(dMart.toString());
+        //reliance.computeEmpWage();
+        //Console.WriteLine(reliance.toString());
+
+        EmpWageBuilderArray empWageBuilder = new EmpWageBuilderArray();
+        empWageBuilder.addCompanyEmpWage("Dmart", 20, 2, 10);
+        empWageBuilder.addCompanyEmpWage("Reliance", 10, 4, 20);
+        empWageBuilder.computeEmpWage();
     }
 }
